f2cf6db535dc79db0ff3d200c0036d90a5508711
Matias Pierobon <matias.pierobonm@gmail.com>
2019-09-09 19:09:18 -0300

    use correct env variables

--- backup.sh
+++ backup.sh
@@ -3,9 +3,9 @@
 NOW=$(date +"%Y-%m-%dT%H:%M:%SZ")
 FILE="/backups/$NOW.sql.gz"
 
-echo "Creating dump of ${DB_NAME} database from ${DB_HOST}..."
+echo "Creating dump of ${MYSQL_DATABASE} database from ${DB_HOST}..."
 TEMP_FILE=$(mktemp)
-mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASSWORD --port=$DB_PORT $DB_NAME > $TEMP_FILE || exit 4
+mysqldump -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD --port=$MYSQL_PORT $MYSQL_DATABASE > $TEMP_FILE || exit 4
 cat $TEMP_FILE | gzip > $FILE || exit 
 echo "SQL backup successfull"
 
@@ -14,13 +14,13 @@ if [ ! -z "$BUCKET" ]; then
   # export AWS_ACCESS_KEY_ID
   # export AWS_SECRET_ACCESS_KEY
   # export AWS_DEFAULT_REGION
-  cat $FILE | aws $AWS_ARGS s3 cp - s3://$BUCKET/$PREFIX/${DB_NAME}_${NOW}.sql.gz || exit 2
+  cat $FILE | aws $AWS_ARGS s3 cp - s3://$BUCKET/$PREFIX/${MYSQL_DATABASE}_${NOW}.sql.gz || exit 2
   echo "SQL backup uploaded successfully"
 fi
 
 if [ ! -z "$MAIL_TO" ]; then
   echo "Sending mail to $MAIL_TO"
-  swaks --header "Subject:Backup of ${DB_NAME} database successfull at ${NOW}" \
+  swaks --header "Subject:Backup of ${MYSQL_DATABASE} database successfull at ${NOW}" \
         --from $MAIL_FROM \
         --server "${MAIL_HOST}" \
         --port "${MAIL_PORT}" \
